5ea1cfe3594c41750647cb1165f0dd65b2ff53ea
Tholut Akhyar <tholut.akhyar@ai-ecom.io>
2022-12-06 21:03:51 +0700

    Dynamic invoice subsribe

--- src/subscribers/invoice.ts
+++ src/subscribers/invoice.ts
@@ -2,18 +2,20 @@ import { EventBusService } from "@medusajs/medusa";
 import InvoiceService from "../services/invoice";
 
 
-class InvoiceNinjaSubscriber {
+class InvoiceSubscriber {
     protected readonly eventBus_: EventBusService
     protected readonly invoice_: InvoiceService
 
-    constructor({ eventBusService, invoiceService }) {
+    constructor({ eventBusService, invoiceService }, options) {
       this.invoice_ = invoiceService
       this.eventBus_ = eventBusService
+      
+      const create_when = options.create_when || "order.payment_captured"
 
-      this.eventBus_.subscribe("order.placed", async (order: any) => {
+      this.eventBus_.subscribe(create_when, async (order: any) => {
         await this.invoice_.createAndGeneratePDFInvoice(order.id)
       });
     }
 }
   
-export default InvoiceNinjaSubscriber;
+export default InvoiceSubscriber;
